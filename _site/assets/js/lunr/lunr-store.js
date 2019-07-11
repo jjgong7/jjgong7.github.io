5b7bc3f6e39f2b63c289981c5b8a6ae5d0310bbf
JJ Gong <jjgong7@gmail.com>
2019-07-11 18:25:09 -0400

    Unsupervised Learning added

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,21 +1,26 @@
 var store = [{
         "title": "Computational Data Analytics Assignments",
-        "excerpt":"Description: Course includes hands-on introduction to programming techniques relevant to data analysis and machine learning. Most of the programming exercises are based on Python and SQL. Notebooks are built “from scratch,” of the basic components of a data analysis pipeline: collection, preprocessing, storage, analysis, and visualization. There are several examples...","categories": [],
+        "excerpt":"Code for this project is available on GitHub. Description: Course includes hands-on introduction to programming techniques relevant to data analysis and machine learning. Most of the programming exercises are based on Python and SQL. Notebooks are built “from scratch,” of the basic components of a data analysis pipeline: collection, preprocessing,...","categories": [],
         "tags": ["machine learning","python","supervised learning"],
         "url": "http://localhost:4000/cda/",
         "teaser":null},{
         "title": "Database Systems - Emergency Resource Management System",
-        "excerpt":"Database Systems - Team Project Team Members: Evan Althouse, Chang-Zhou Gong, Tich Mangono, David Ribeiro Languages: Python, SQL, Javascript, HTML, CSS Libraries: jQuery Frameworks: Flask, Bootstrap Relational Database Management System: MariaDB (MySQL) Overview: Emergency Resource Management System (ERMS) Build an information management tool that supports government agencies and municipalities in...","categories": [],
+        "excerpt":"Database Systems - Team Project Code for this project is available on GitHub. Team Members: Evan Althouse, Chang-Zhou Gong, Tich Mangono, David Ribeiro Languages: Python, SQL, Javascript, HTML, CSS Libraries: jQuery Frameworks: Flask, Bootstrap Relational Database Management System: MariaDB (MySQL) Overview: Emergency Resource Management System (ERMS) Build an information management...","categories": [],
         "tags": ["python","flask","sql"],
         "url": "http://localhost:4000/dbm/",
         "teaser":null},{
         "title": "Space Shuttle Challenger",
-        "excerpt":" ","categories": [],
+        "excerpt":"Code for this project is available on GitHub.  ","categories": [],
         "tags": ["bayesian statistics","WinBUGS"],
         "url": "http://localhost:4000/bay/",
         "teaser":null},{
+        "title": "Unsupervised Learning",
+        "excerpt":"Objective: Details for this project are available on GitHub. Six different algorithms are implemented; the first two are clustering – k-means clustering and Expectation Maximization and the last four are dimensionality reduction algorithms – PCA, ICA, Randomized Projections, and Random Forest. The experiments are split into four main parts. All...","categories": [],
+        "tags": ["unsupervised learning","python","jython"],
+        "url": "http://localhost:4000/ul/",
+        "teaser":null},{
         "title": "Supervised Learning",
-        "excerpt":"Analyze two datasets using five different supervised learning algorithms. Weka, a suite of machine learning software written in Java, is used for analysis. Datasets used: Faulty Steel Plates 1,941 instances; 27 attributes; 7 labels Steel Plates are classified into 7 different faulty categories - Pastry, Z_Scratch, K_Scratch, Stains, Dirtiness, Bumps,...","categories": [],
+        "excerpt":"Objective: Details for this project are available on GitHub. Analyze two datasets using five different supervised learning algorithms. Weka, a suite of machine learning software written in Java, is used for analysis. Datasets used: Faulty Steel Plates 1,941 instances; 27 attributes; 7 labels Steel Plates are classified into 7 different...","categories": [],
         "tags": ["supervised learning","python"],
         "url": "http://localhost:4000/sl/",
         "teaser":null}]
