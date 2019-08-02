af02e0fd880da45a9aa79ae55e4283f21f84d0b7
JJ Gong <jjgong7@gmail.com>
2019-08-02 17:16:47 -0400

    DVA Uploaded

--- _pages/about.md
+++ _pages/about.md
@@ -4,7 +4,9 @@ permalink: /about/
 author_profile: true
 ---
 
-Data scientist with 1.5+ years’ experience in project work. Skilled in machine learning, statistics, and programming. Proficient in Python, R, and SQL. 
-Strong curiosity to learn, highly coachable, and excited to bring value to your team.
+* Data scientist with 1.5+ years’ experience in project work.  
+* Skilled in machine learning, statistics, and programming.  
+* Proficient in Python, R, and SQL.  
+* Strong curiosity to learn, highly coachable, and excited to bring value to your team.
 
-Hobbies include basketball, weight lifting, cooking, astronomy.
+In my free time I enjoy playing basketball, lifting weights, cooking healthy meals, exploring national parks, and trying different pizza and noodle places around New York City!  
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,9 @@
 var store = [{
+        "title": "Data and Visual Analytics",
+        "excerpt":"Description: Four assignments demonstrating techniques and tools for analyzing and visualizing data at scale. Languages &amp; Frameworks used: JavaScript (D3.js), Python, Java, Scala, Pig, SQL, HTML, CSS, Hadoop, Spark, and AWS. Data and Visual Analytics Assignments: HW1-Python, SQLite, D3, Gephi, and OpenRefine HW2-D3 Graphs and Visualization HW3-Hadoop, Spark, Pig, AWS...","categories": [],
+        "tags": ["python","javascript","SQL","D3.js","AWS","Big Data"],
+        "url": "http://localhost:4000/dva/",
+        "teaser":null},{
         "title": "Computational Data Analytics Assignments",
         "excerpt":"Code for this project is available on GitHub. Description: Course includes hands-on introduction to programming techniques relevant to data analysis and machine learning. Most of the programming exercises are based on Python and SQL. Notebooks are built “from scratch,” of the basic components of a data analysis pipeline: collection, preprocessing,...","categories": [],
         "tags": ["machine learning","python","supervised learning"],
@@ -14,6 +19,11 @@ var store = [{
         "tags": ["bayesian statistics","WinBUGS"],
         "url": "http://localhost:4000/bay/",
         "teaser":null},{
+        "title": "MORE Application",
+        "excerpt":"Details:  Code for this project is available on GitHub.   Deployed project on AWS Elastic Beanstalk is available here.   Poster:       ","categories": [],
+        "tags": ["python","javascript","SQL","D3.js"],
+        "url": "http://localhost:4000/more/",
+        "teaser":null},{
         "title": "Unsupervised Learning",
         "excerpt":"Objective: Details for this project are available on GitHub. Six different algorithms are implemented; the first two are clustering – k-means clustering and Expectation Maximization and the last four are dimensionality reduction algorithms – PCA, ICA, Randomized Projections, and Random Forest. The experiments are split into four main parts. All...","categories": [],
         "tags": ["unsupervised learning","python","jython"],
@@ -25,7 +35,7 @@ var store = [{
         "url": "http://localhost:4000/sl/",
         "teaser":null},{
         "title": "Practicum - NHANES",
-        "excerpt":"Objective: The objective of the practicum is to identify the prevalence and risk factors of hospital utilization and major diseases over time using publicly reported qualitative and quantitative data. To help answer this objective, I will explore five main questions: What factors predict the likelihood of a patient utilizing hospital...","categories": [],
+        "excerpt":"Objective: Details for this project are available on GitHub. The objective of the practicum is to identify the prevalence and risk factors of hospital utilization and major diseases over time using publicly reported qualitative and quantitative data. To help answer this objective, I will explore five main questions: What factors...","categories": [],
         "tags": ["practicum","python","R","supervised learning"],
         "url": "http://localhost:4000/nhanes/",
         "teaser":null}]
